12008a8f57e28b138f22e1fb92e845c626587876
nomand <alexey.botkov@gmail.com>
2018-06-20 11:38:46 +1200

    part 3

--- Assets/Scripts/KochGenerator.cs
+++ Assets/Scripts/KochGenerator.cs
@@ -21,8 +21,23 @@ public class KochGenerator : MonoBehaviour {
         Octagon
     };
 
+    public struct LineSegment
+    {
+        public Vector3 StartPosition { get; set; }
+        public Vector3 EndPosition { get; set; }
+        public Vector3 Direction { get; set; }
+        public float Length { get; set; }
+    }
+
     [SerializeField]
     protected _initiator initiator = new _initiator();
+
+    [SerializeField]
+    protected AnimationCurve generator;
+    protected Keyframe[] keys;
+
+    protected int generationCount;
+
     [SerializeField]
     protected _axis axis = new _axis();
 
@@ -35,11 +50,34 @@ public class KochGenerator : MonoBehaviour {
     private Vector3 rotateAxis;
 
     protected Vector3[] position;
+    protected Vector3[] targetPosition;
+    private List<LineSegment> lineSegment;
+
+    private void Awake()
+    {
+        GetInitiatorPoins();
+
+        position = new Vector3[initiatorPointAmount + 1];
+        targetPosition = new Vector3[initiatorPointAmount + 1];
+        keys = generator.keys;
+
+        rotateVector = Quaternion.AngleAxis(initialRotation, rotateAxis) * rotateVector;
+
+        for (int i = 0; i < initiatorPointAmount; i++)
+        {
+            position[i] = rotateVector * initiatorSize;
+            rotateVector = Quaternion.AngleAxis(360 / initiatorPointAmount, rotateAxis) * rotateVector;
+        }
+        position[initiatorPointAmount] = position[0];
+        targetPosition = position;
+    }
 
     private void OnDrawGizmos()
     {
         GetInitiatorPoins();
+        // assign lists and arrays
         initiatorPoint = new Vector3[initiatorPointAmount];
+        lineSegment = new List<LineSegment>();
 
         rotateVector = Quaternion.AngleAxis(initialRotation, rotateAxis) * rotateVector;
 
@@ -48,6 +86,7 @@ public class KochGenerator : MonoBehaviour {
             initiatorPoint[i] = rotateVector * initiatorSize;
             rotateVector = Quaternion.AngleAxis(360 / initiatorPointAmount, rotateAxis) * rotateVector;
         }
+
         for (int i = 0; i < initiatorPointAmount; i++)
         {
             Gizmos.color = Color.white;
@@ -62,6 +101,58 @@ public class KochGenerator : MonoBehaviour {
                 Gizmos.DrawLine(initiatorPoint[i], initiatorPoint[0]);
             }
         }
+    }
+
+    protected void GenerateKoch(Vector3[] positions, bool outwards, float generatorMultiplier)
+    {
+        //creating line segments
+        lineSegment.Clear();
+        for (int i = 0; i < positions.Length - 1; i++)
+        {
+            LineSegment line = new LineSegment();
+            line.StartPosition = positions[i];
+            if(i==positions.Length-1)
+                line.EndPosition = positions[0];
+            else
+                line.EndPosition = positions[i + 1];
+
+            line.Direction = (line.EndPosition - line.StartPosition).normalized;
+            line.Length = Vector3.Distance(line.EndPosition, line.StartPosition);
+            lineSegment.Add(line);
+        }
+
+        List<Vector3> newPos = new List<Vector3>();
+        List<Vector3> targetPos = new List<Vector3>();
+
+        for (int i = 0; i < lineSegment.Count; i++)
+        {
+            newPos.Add(lineSegment[i].StartPosition);
+            targetPos.Add(lineSegment[i].StartPosition);
+
+            for (int j = 1; j < keys.Length - 1; j++)
+            {
+                float moveAmount = lineSegment[i].Length * keys[j].time;
+                float heightAmount = (lineSegment[i].Length * keys[j].value) * generatorMultiplier;
+                Vector3 movePos = lineSegment[i].StartPosition + (lineSegment[i].Direction * moveAmount);
+                Vector3 direction;
+                if(outwards)
+                    direction = Quaternion.AngleAxis(-90, rotateAxis) * lineSegment[i].Direction;
+                else
+                    direction = Quaternion.AngleAxis(90, rotateAxis) * lineSegment[i].Direction;
+                newPos.Add(movePos);
+                targetPos.Add(movePos + (direction * heightAmount));
+            }
+        }
+
+        newPos.Add(lineSegment[0].StartPosition);
+        targetPos.Add(lineSegment[0].StartPosition);
+
+        position = new Vector3[newPos.Count];
+        targetPosition = new Vector3[targetPos.Count];
+        position = newPos.ToArray();
+        targetPosition = targetPos.ToArray();
+
+        generationCount++;
     }
 
     private void GetInitiatorPoins()
@@ -118,28 +209,4 @@ public class KochGenerator : MonoBehaviour {
                 break;
         }
     }
-
-    private void Awake()
-    {
-        GetInitiatorPoins();
-        position = new Vector3[initiatorPointAmount + 1];
-
-        rotateVector = Quaternion.AngleAxis(initialRotation, rotateAxis) * rotateVector;
-
-        for (int i = 0; i < initiatorPointAmount; i++)
-        {
-            position[i] = rotateVector * initiatorSize;
-            rotateVector = Quaternion.AngleAxis(360 / initiatorPointAmount, rotateAxis) * rotateVector;
-        }
-        position[initiatorPointAmount] = position[0];
-    }
-
-    void Start ()
-    {
-		
-	}
-	
-	void Update () {
-		
-	}
 }
--- Assets/Scripts/KochLine.cs
+++ Assets/Scripts/KochLine.cs
@@ -6,16 +6,47 @@ using UnityEngine;
 public class KochLine : KochGenerator
 {
     LineRenderer lineRenderer;
+    [Range(0,1)]
+    public float LerpAmount;
+    Vector3[] lerpPosition;
+    public float generateMultiplier;
 
 	void Start ()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = true;
+        lineRenderer.useWorldSpace = false;
+        lineRenderer.loop = true;
         lineRenderer.positionCount = position.Length;
         lineRenderer.SetPositions(position);
 	}
 	
 	void Update ()
     {
-		
-	}
+        if(generationCount != 0)
+        {
+            for(int i = 0; i < position.Length; i++)
+            {
+                lerpPosition[i] = Vector3.Lerp(position[i], targetPosition[i], LerpAmount);
+            }
+            lineRenderer.SetPositions(lerpPosition);
+        }
+
+		if(Input.GetKeyDown(KeyCode.O))
+        {
+            GenerateKoch(targetPosition, true, generateMultiplier);
+            lerpPosition = new Vector3[position.Length];
+            lineRenderer.positionCount = position.Length;
+            lineRenderer.SetPositions(position);
+            LerpAmount = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            GenerateKoch(targetPosition, false, generateMultiplier);
+            lerpPosition = new Vector3[position.Length];
+            lineRenderer.positionCount = position.Length;
+            lineRenderer.SetPositions(position);
+            LerpAmount = 0;
+        }
+    }
 }
